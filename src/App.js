f05c4ee45168d3a31a26adcb91bf98d8527cb406
aditigupta104 <aditiguptainfo09@gmail.com>
2026-01-04 21:37:30 +0530

    Completed first page till curved design

--- src/App.js
+++ src/App.js
@@ -21,6 +21,20 @@ function App(){
         <h2 className='phrase'>Crude to Chemical</h2>
         <h1 className="title">ALCHEMY'26</h1>
     </section>
+    <section className="fest-part">
+      <h2 className="heading">Be a part of our <span>Fest!</span> </h2>
+      <div className="content">
+        <p className="text">
+          From crude oil to life-saving medicines, from raw materials to revolutionary products—chemical engineering is the alchemy of the modern world. Be part of the transformation at Alchemy '26, where innovation meets tradition.
+        </p>
+        <div className="info">
+          <div className="dtg">96 Days to go</div>
+          <div className="edition"><p>10th Edition</p>
+          <p>2026</p></div>
+        </div>
+
+      </div>
+    </section>
     </div>
   );
 }
